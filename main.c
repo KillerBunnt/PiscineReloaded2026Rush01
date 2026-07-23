e228a5e9efd56a135f8c57dea8b769f1be37b8d9
joloo <joloo@student.42kl.edu.my>
2026-07-23 19:53:47 +0800

    initialize grid to zeros and a debug function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 18:55:41 by joloo             #+#    #+#             */
-/*   Updated: 2026/07/23 19:20:29 by joloo            ###   ########.fr       */
+/*   Updated: 2026/07/23 19:38:46 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int	main(int argc, char **argv)
 {
-	int grid[6][6];
+	int grid[6][6] = {0};
 
 	if (parsing(argc, argv, grid) == -1)
 		return (1);
+	if (DEBUG_FLAG == 1)
+		debug_grid(grid);
 	return (0);
 }

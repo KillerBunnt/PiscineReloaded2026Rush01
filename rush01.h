833dcdf9f1b123240c8ab0d5e332e0b95196a2d4
joloo <joloo@student.42kl.edu.my>
2026-07-23 19:22:30 +0800

    refactor: simplify defines

--- rush01.h
+++ rush01.h
@@ -6,36 +6,17 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 18:55:39 by joloo             #+#    #+#             */
-/*   Updated: 2026/07/23 19:11:05 by joloo            ###   ########.fr       */
+/*   Updated: 2026/07/23 19:22:19 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RUSH01_H
  #define RUSH01_H
 
-#ifndef TOP_ROW
 # define TOP_ROW 1
-# endif
-
-#ifndef BOT_ROW
 # define BOT_ROW 4
-# endif
-
-#ifndef LEFT_COL
 # define LEFT_COL 1
-# endif
-
-#ifndef RIGHT_COL
 # define RIGHT_COL 4
-# endif
-
-#ifndef TRUE
-# define TRUE 1
-# endif
-
-#ifndef FALSE
-# define FALSE 0
-# endif
 
 // typedef struct s_data
 // {
@@ -43,5 +24,9 @@
 
 // } t_data;
 
+
+int	parsing(int argc, char **argv, char grid[6][6]);
+
+void	print_error(void);
 
 #endif

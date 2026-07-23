0b5c90513a5ec8c0e4ca5e527844811efc405a83
joloo <joloo@student.42kl.edu.my>
2026-07-23 19:23:40 +0800

    feat: add a define for max tower height

--- rush01.h
+++ rush01.h
@@ -6,7 +6,7 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 18:55:39 by joloo             #+#    #+#             */
-/*   Updated: 2026/07/23 19:22:19 by joloo            ###   ########.fr       */
+/*   Updated: 2026/07/23 19:23:23 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define LEFT_COL 1
 # define RIGHT_COL 4
 
+# define MAX_HEIGHT 4
 // typedef struct s_data
 // {
 // 	char	grid[6][6];

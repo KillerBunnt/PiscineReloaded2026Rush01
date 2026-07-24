0f58ea798f8dde85c52369268ac9fb2e14039beb
joloo <joloo@student.42kl.edu.my>
2026-07-24 16:57:49 +0800

    feat:  add optional string message to print_error

--- ex00/utils.c
+++ ex00/utils.c
@@ -6,15 +6,17 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 19:09:44 by joloo             #+#    #+#             */
-/*   Updated: 2026/07/24 16:44:51 by joloo            ###   ########.fr       */
+/*   Updated: 2026/07/24 16:57:22 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush01.h"
 
-void	print_error(void)
+void	print_error(char *str)
 {
 	write(1, "Error\n", 6);
+	if (str != NULL)
+		write(1, str, ft_strlen(str));
 }
 
 void	ft_putchar(char c)

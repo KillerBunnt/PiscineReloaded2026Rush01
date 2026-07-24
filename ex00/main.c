844bad29213b48da4323bf17d743a591f7ed5bf2
joloo <joloo@student.42kl.edu.my>
2026-07-24 17:09:38 +0800

    feat: add the algorithm and print_grid to main()

--- ex00/main.c
+++ ex00/main.c
@@ -6,11 +6,33 @@
 /*   By: joloo <joloo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 18:55:41 by joloo             #+#    #+#             */
-/*   Updated: 2026/07/24 16:46:58 by joloo            ###   ########.fr       */
+/*   Updated: 2026/07/24 17:07:22 by joloo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rush01.h"
+
+static void	print_grid(int grid[6][6])
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while (x < 6)
+	{
+		ft_putchar(grid[y][x] + '0');
+		ft_putchar(',');
+		ft_putchar(' ');
+		y++;
+		if (y >= 6)
+		{
+			ft_putchar('\n');
+			y = 0;
+			x++;
+		}
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -20,5 +42,7 @@ int	main(int argc, char **argv)
 		return (1);
 	if (DEBUG_FLAG == 1)
 		debug_grid(grid);
+	generate_solution(1, 1, 4, grid);
+	print_grid(grid);
 	return (0);
 }
